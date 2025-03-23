dbcbb094516413cdc23cb30f637c7864a972d920
Thimira Vimukthi <ictthimira@gmail.com>
2025-03-23 11:20:50 +0530

    first commit

---
+++ src/main/java/com/jobskillportal/jobskillportalbackend/JobskillportalbackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobskillportal.jobskillportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobskillportalbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobskillportalbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobskillportal/jobskillportalbackend/JobskillportalbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobskillportal.jobskillportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobskillportalbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
